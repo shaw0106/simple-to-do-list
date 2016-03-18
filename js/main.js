9d443b5ad4587cf4a5a913d2989c8e09326e8f6b
Shannon <shaw0106@algonquinlive.com>
2016-03-18 16:03:53 -0400

    Started

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 var $input = $('#grocery-input');
-var $list = $(".list");
+var $list = $('.list');
 
 $('form').on('submit', function (e) {
   var $li = $('<li>');
+
   e.preventDefault();
   $li.html($input.val());
   $list.append($li);
+  $input.val('');
+});
+
+$('ul').on('click', 'li', function (e) {
+  $(this).toggleClass('is-clicked');
 });
